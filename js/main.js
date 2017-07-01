93f84d603839db53eff8124a2250667213aea38c
Matt Woods <mattwoods21@gmail.com>
2017-07-01 01:05:52 -0500

    trying to use video

--- js/main.js
+++ js/main.js
@@ -10,5 +10,7 @@ for (var i=bottles; i > 0; i--){
 
 	lyrics = i + " bottles of " + drink + " on the wall, " + i + " bottles of " + drink + ", take one down, pass it around, " + (i - 1) + " bottles of " + drink + " on the wall!"
 
-	holdMyBeer.innerHTML += lyrics + "<br/><br/>";		
+	holdMyBeer.innerHTML += lyrics + "<br/><br/>";
+
 }
+
