8b7d13e921f625bcf89ca8fd4bde3482788c3372
Matt Woods <mattwoods21@gmail.com>
2017-06-30 19:59:38 -0500

    got it to count up in node

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var verse1 = " bottles of beer on the wall, "
+var verse2 = " bottles of beer, take one down, pass it around, "
+var verse3 = " bottles of beer on the wall!"
+
+var bottles = 1
+
+while (bottles <= 99) {
+	console.log(
+		bottles + verse1 + bottles + verse2 + bottles + verse3
+		)
+	bottles = bottles + 1
+};
+// else {
+// 	console.log("We're all done here!")
+// };
