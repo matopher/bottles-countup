0fd50a55fa94cc402dde6562e4a0df2e6fbfd8da
Matt Woods <mattwoods21@gmail.com>
2017-06-30 20:02:20 -0500

    now counting down to node, added supersonic reference

--- js/main.js
+++ js/main.js
@@ -2,14 +2,16 @@ var verse1 = " bottles of beer on the wall, "
 var verse2 = " bottles of beer, take one down, pass it around, "
 var verse3 = " bottles of beer on the wall!"
 
-var bottles = 1
+var bottles = 99
 
-while (bottles <= 99) {
+while (bottles >= 1) {
 	console.log(
 		bottles + verse1 + bottles + verse2 + bottles + verse3
 		)
-	bottles = bottles + 1
+	bottles = bottles - 1
 };
+
+console.log("No more beer here! *Cue Closing Time by Supersonic*")
 // else {
 // 	console.log("We're all done here!")
 // };
